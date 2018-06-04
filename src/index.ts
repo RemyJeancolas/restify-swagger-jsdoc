d8c7ec60385d674cb6a05c0ca6760d7f22703bb7
Mitchell Bundy <mitchell.bundy@ssense.com>
2018-06-04 18:12:49 -0400

    updated paths to find-my-way-esque strings

--- src/index.ts
+++ src/index.ts
@@ -70,19 +70,20 @@ export function createSwaggerPage(options: SwaggerPageOptions): void {
         return next();
     });
 
-    options.server.get(new RegExp(publicPath + '\/?$'), (req, res, next) => {
+    options.server.get(`${publicPath}`, (req, res, next) => {
         res.setHeader('Location', `${publicPath}/index.html`);
         res.send(302);
         return next();
     });
 
-    options.server.get(new RegExp(publicPath + '\/(.*)$'), (req, res, next) => {
-        fs.readFile(path.resolve(swaggerUiPath, req.params[0]), (err, content) => {
+    options.server.get(`${publicPath}/*`, (req, res, next) => {
+        const file = req.params['*'];
+        fs.readFile(path.resolve(swaggerUiPath, file), (err, content) => {
             if (err) {
-                return next(new errors.NotFoundError(`File ${req.params[0]} does not exist`));
+                return next(new errors.NotFoundError(`File ${file} does not exist`));
             }
 
-            if (req.params[0] === 'index.html') {
+            if (file === 'index.html') {
                 const isReqSecure = options.forceSecure || req.isSecure();
                 const jsonFileUrl = `${isReqSecure ? 'https' : 'http'}://${req.headers.host}${publicPath}/swagger.json`;
                 content = new Buffer(content.toString().replace(
@@ -91,7 +92,7 @@ export function createSwaggerPage(options: SwaggerPageOptions): void {
                 ));
             }
 
-            const contentType = mime.lookup(req.params[0]);
+            const contentType = mime.lookup(file);
             if (contentType !== false) {
                 res.setHeader('Content-Type', contentType);
             }
