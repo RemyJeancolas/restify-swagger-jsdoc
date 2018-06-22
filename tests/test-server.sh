ff9808ac5ff10d510779e4cab64c949876d9bbd9
Mitchell Bundy <mitchell.bundy@ssense.com>
2018-06-22 14:18:45 -0400

    update to restify 7, tests should fail

--- tests/test-server.sh
+++ tests/test-server.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 MOCHA_PATH=./node_modules/.bin/mocha
 
-npm install restify@^4 --no-save --progress=false
-$MOCHA_PATH tests --require ts-node/register --bail ./tests/server.ts
-
-npm install restify@^5 --no-save --progress=false
-$MOCHA_PATH tests --require ts-node/register --bail ./tests/server.ts
-
-npm install restify@^6 --no-save --progress=false
+npm install restify@^7 --no-save --progress=false
 $MOCHA_PATH tests --require ts-node/register --bail ./tests/server.ts
