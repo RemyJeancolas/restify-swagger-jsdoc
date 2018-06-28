11e274f579d965c50b0a56536a69136d951014ce
zenit1scada <serge@scadafence.com>
2018-06-28 09:15:11 +0300

    security definitions in index.js

--- js/index.js
+++ js/index.js
@@ -19,29 +19,36 @@ function createSwaggerPage(options) {
         throw new Error('options.path is required');
     }
     const swaggerUiPath = path.dirname(require.resolve('swagger-ui'));
-    const swaggerSpec = swaggerJSDoc({
-        swaggerDefinition: {
-            info: {
-                title: options.title,
-                version: options.version,
-                description: typeof options.description === 'string' ? options.description : undefined
-            },
-            host: typeof options.host === 'string' ? options.host.replace(/\/+$/, '') : undefined,
-            basePath: typeof options.routePrefix === 'string' ? `/${options.routePrefix.replace(/^\/+/, '')}` : '/',
-            schemes: Array.isArray(options.schemes) ? options.schemes : undefined,
-            tags: Array.isArray(options.tags) ? options.tags : [],
-	        securityDefinitions:{}
-        },
-        apis: Array.isArray(options.apis) ? options.apis : []
-    });
+
+    let securityDefinitions = {};
+
+	if (options.securityDefinitions) {
+		Object.keys(options.securityDefinitions).forEach(key => {
+			securityDefinitions[key] = options.securityDefinitions[key];
+		});
+	}
+
+    const swaggerOptions = {
+	    swaggerDefinition: {
+		    info: {
+			    title: options.title,
+			    version: options.version,
+			    description: typeof options.description === 'string' ? options.description : undefined
+		    },
+		    host: typeof options.host === 'string' ? options.host.replace(/\/+$/, '') : undefined,
+		    basePath: typeof options.routePrefix === 'string' ? `/${options.routePrefix.replace(/^\/+/, '')}` : '/',
+		    schemes: Array.isArray(options.schemes) ? options.schemes : undefined,
+		    tags: Array.isArray(options.tags) ? options.tags : [],
+		    securityDefinitions:securityDefinitions
+	    },
+	    apis: Array.isArray(options.apis) ? options.apis : []
+    };
+
+    const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
     if (options.definitions) {
         Object.keys(options.definitions).forEach(key => {
             swaggerSpec.definitions[key] = options.definitions[key];
-        });
-    }
-    if (options.securityDefinitions) {
-        Object.keys(options.securityDefinitions).forEach(key => {
-            swaggerSpec.swaggerDefinition.securityDefinitions[key] = options.securityDefinitions[key];
         });
     }
     const publicPath = options.path.replace(/\/+$/, '');
